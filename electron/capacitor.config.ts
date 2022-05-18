60bd1a15d07756fb142abf81e8955a4023430e71
Isac <isac.as0001@gmail.com>
2022-05-18 13:06:31 +0100

    Followed Sqlite capacitor installation guide

---
+++ electron/capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: '40978-PWM-Sprint-4-Ionic-Angular',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
